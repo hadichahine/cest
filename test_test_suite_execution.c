a7280fcfcae6b6afc0afb411a4d703721480b8b5
Hadi Chahine <hadichahine232@hotmail.com>
2019-05-31 20:57:25 +0300

    Add test suite execution test.

---
+++ test_test_suite_execution.c
@@ -0,0 +1,12 @@
+#include "cutest.h"
+#include "cutest_assert.h"
+#include "primitive_testing_environment.h"
+
+
+void testNothing(){
+
+}
+
+void main(){
+    runPrimitiveTest("testNothing",testNothing);
+}

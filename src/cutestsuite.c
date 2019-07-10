2967a7900bcd93de9a02c127a4e513f9407a9e8e
Hadi Chahine <hadichahine232@hotmail.com>
2019-07-10 20:41:31 +0300

    Remove redundant code from test suite implementation.

--- src/cutestsuite.c
+++ src/cutestsuite.c
@@ -3,28 +3,17 @@
 #include <stdlib.h>
 
 typedef struct {
-    CUTest *test;
-    CUTest *secondTest;
-    CUTest *thirdTest;
     LinkedList *testsList;
 } CUTestSuite;
 
 CUTestSuite *CUTestSuite_create(char *name){
     CUTestSuite *testSuite = (CUTestSuite*)malloc(sizeof(CUTestSuite));
-    testSuite->test = NULL;
-    testSuite->secondTest = NULL;
-    testSuite->thirdTest = NULL;
     testSuite->testsList = createLinkedList();
     return testSuite;
 }
 
 void CUTestSuite_addTest(CUTestSuite *testSuite,CUTest *test){
     addItemToLinkedList(testSuite->testsList,test);
-    if(!testSuite->test)
-        testSuite->test = test;
-    else if(!testSuite->secondTest)
-            testSuite->secondTest = test;
-        else testSuite->thirdTest = test;
 }
 
 void CUTestSuite_execute(CUTestSuite *testSuite){
